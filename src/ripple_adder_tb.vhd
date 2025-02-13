40851ea41bc1b6a7b8b99b678562b6cd90d5e9b1
jplovanich76 <c27jack.plovanich@afacademy.af.edu>
2025-02-13 09:17:47 -0700

    x

--- src/ripple_adder_tb.vhd
+++ src/ripple_adder_tb.vhd
@@ -50,8 +50,11 @@ begin
 	       assert (w_sum = x"F" and w_Cout = '1') report "bad with ones" severity failure;
        -- TODO, a few other test cases
        w_addends <= x"11"; w_Cin <= '1'; wait for 10 ns;
-	       assert (w_sum = x"3" and w_Cout = '0') report "error - 1, 1, 1" severity failure;
+	       assert (w_sum = x"3" and w_Cout = '0') report "error" severity failure;
+	   w_addends <= x"FF"; w_Cin <= '0'; wait for 10ns;
+	       assert (w_sum = x"E" and w_Cout = '1') report "error" severity failure;
 	end process;	
+	   
 	-----------------------------------------------------	
 	
 end test_bench;
